a59c9e58adac26d36abec4ff717084309a495ab0
Ankit Meena <ankitmeena@Ankits-MacBook-Pro.local>
2023-10-26 22:05:27 +0530

    convex and Clerk setup

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+export default {
+  providers: [
+    {
+      domain: "https://stable-frog-65.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+// https://stable-frog-65.clerk.accounts.dev
