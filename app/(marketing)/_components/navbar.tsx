2b59ae1005170c1ae1aa93b5882d46f31240ef36
Ankit Meena <ankitmeena@Ankits-MacBook-Pro.local>
2023-10-27 17:01:15 +0530

    Add Authentication

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,15 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Ghost } from "lucide-react";
+import { Spinner } from "@/components/spinner";
+import { Link } from "lucide-react";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -17,7 +24,28 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto  md:justify-end  justify-between w-full flex items-center gap-x-2">
-       <ModeToggle/>
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton>
+              <Button variant={"ghost"} size={"sm"}>
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton>
+              <Button size={"sm"}>Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant={"ghost"} size={"sm"} asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+        <ModeToggle />
       </div>
     </div>
   );
