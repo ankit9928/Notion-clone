8073bfd2798d25bed7f41bf4576c4354cb57a6bc
ankit <102666592+ankit9928@users.noreply.github.com>
2023-10-31 18:32:43 +0530

    New branch (#9)
    * Add sidebar Navbar event
    
    * Add sidebar dropdown menu
    
    * Side bar Completed
    
    * Add Schema and APi
    
    ---------
    
    Co-authored-by: Ankit Meena <ankitmeena@Ankits-MacBook-Pro.local>

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+
+const DocumentsPage = () => {
+  const { user } = useUser();
+  const Create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = Create({title: "Untitled"});
+
+    toast.promise(promise,{
+      loading: "Creating a new note....",
+      success: "New note created",
+      error: "Failed to create a new note."
+    })
+  };
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src={"/empty.png"}
+        height={"300"}
+        width={"300"}
+        alt="empty"
+        className="dark:hidden"
+      />
+      <Image
+        src={"/empty-dark.png"}
+        height={"300"}
+        width={"300"}
+        alt="empty"
+        className="hidden dark:block"
+      />
+      <h2>Welcome to {user?.firstName}&apos;s Jotion</h2>
+      <Button onClick={onCreate}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
+};
+
+export default DocumentsPage;
