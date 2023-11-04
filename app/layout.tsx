5c3bbb2caa4385097277cb02affba87bc69020d0
Ankit Meena <ankitmeena@Ankits-MacBook-Pro.local>
2023-11-04 16:40:33 +0530

    ADD Settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provide";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -44,6 +45,7 @@ export default function RootLayout({
             storageKey="jotion-theme-2"
           >
             <Toaster position="bottom-center" />
+            <ModalProvider/>
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
