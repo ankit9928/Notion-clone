8918e6eac739b34f3469abbfc8750b0c15d8e4f6
Ankit Meena <ankitmeena@Ankits-MacBook-Pro.local>
2023-11-07 23:34:09 +0530

    Add Delete Replace cover Image

--- components/ui/cover.tsx
+++ components/ui/cover.tsx
@@ -9,6 +9,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
   url?: string;
@@ -18,10 +19,16 @@ interface CoverImageProps {
 export const Cover = ({ url, preview }: CoverImageProps) => {
   const coverImage = useCoverImage();
   const params = useParams();
+  const { edgestore } = useEdgeStore();
 
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url
+      });
+    }
     removeCoverImage({
       id: params.documentId as Id<"documents">,
     });
@@ -39,13 +46,13 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
           >
             <ImageIcon className="h-4 w-4 mr-2" />
-            Change cover 
+            Change cover
           </Button>
 
           <Button
