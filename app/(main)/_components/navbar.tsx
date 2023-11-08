2b67dc69c6e61a61edaba38a2b7c8f97dba18904
Ankit Meena <ankitmeena@Ankits-MacBook-Pro.local>
2023-11-08 20:27:28 +0530

    Preview functionality

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -5,6 +5,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { MenuIcon } from "lucide-react";
 import { Title } from "./title";
 import { Banner } from "./banner";
+import { Publish } from "./publish";
 import { Menu } from "./menu";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
@@ -49,6 +50,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
